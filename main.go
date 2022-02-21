bd147477cf1b2cc33cc932db0cc130c70b63e256
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2022-02-21 08:54:10 +0100

    Slices and FOR loop.

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
+	// Declare a slice of strings
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
 
-	fmt.Println(card)
+	fmt.Println(cards)
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
